1161e17ce961ae31c27efe5543d5313ef200de03
Sisi Wu <sw223hk@student.lnu.se>
2025-11-21 00:18:38 +0100

    Refcator -- BugDoesNotTrim()

--- java/practice/practice_with_run_tests/app/src/main/java/com/lab/BugDoesNotTrim.java
+++ java/practice/practice_with_run_tests/app/src/main/java/com/lab/BugDoesNotTrim.java
@@ -3,21 +3,21 @@ package com.lab;
 // Buggy 1: Does not trim whitespace
 class BugDoesNotTrim implements IPassword {
     protected int passwordHash;
-    private String trimmed_pw; // trimmed password for testing
+    private String trimmedPassword; // trimmed password for testing
     
     // Trim white space, check length (>12), and contain a number
     public BugDoesNotTrim(String pw) throws Exception {
         // Bug: Don't trim whitespace
-        String trimmed_pw = pw.replaceAll("\\s", ""); //remove all space
+        String trimmed_password = pw.replaceAll("\\s", ""); // remove all space 
 
-        if (isToShort(trimmed_pw)) {
+        if (isToShort(trimmed_password)) {
             throw new Exception("To short password");
         }
-        if (containsNumber(trimmed_pw) == false) {
+        if (containsNumber(trimmed_password) == false) {
             throw new Exception("Does not contain a number");
         }
-        this.passwordHash = simpleHash(trimmed_pw);
-        this.trimmed_pw = trimmed_pw;
+        this.passwordHash = simpleHash(trimmed_password); // store hash value of pw in passwordHash 
+        this.trimmedPassword = trimmed_password; // stores trimmed_password in trimmed_pw
     }
     
     private int simpleHash(String input) {
@@ -42,7 +42,7 @@ class BugDoesNotTrim implements IPassword {
 
 
     public String getTrimmedPassword() {
-        return trimmed_pw;
+        return trimmedPassword;
     }
     
     public boolean isPasswordSame(IPassword other) {
