493b292da97dfcb71f979d34ae74489543abb4a4
Sisi Wu <sw223hk@student.lnu.se>
2025-11-22 01:08:48 +0100

    Test Refactor -- passwordLengthCheckMissing()

--- java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
+++ java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
@@ -81,6 +81,6 @@ public class PasswordTest {
    @Test
    public void passwordLengthCheckMissing() throws Exception {
        BugMissingPasswordLengthCheck password = new BugMissingPasswordLengthCheck("password1"); // contain a number to meet another condition
-       assertNotNull(password); 
+       assertNotNull(password); // password is created with unchecked length, which means password not null
    }
 }
