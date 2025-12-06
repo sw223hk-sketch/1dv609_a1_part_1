20912eb64340d2edbbfb5079312b2f8bcbf3cffe
Sisi Wu <sw223hk@student.lnu.se>
2025-12-06 20:39:21 +0100

    testGetQuestion() RED

---
+++ java/a1p2/app/src/test/java/org/example/FlashcardTest.java
@@ -0,0 +1,17 @@
+/*
+ * This is FlashcardTest class.
+ */
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FlashcardTest {
+
+    @Test
+    void testGetQuestion() {
+        Flashcard fc = new Flashcard("Hi in Swedish?", "Hej");
+        assertEquals("Hi in Swedish", fc.getQuestion());
+    }
+
+}
