9d7ff4ad64dcef9ad2605815305b2f9fbf4dc91b
Sisi Wu <sw223hk@student.lnu.se>
2025-12-09 12:20:06 +0100

    FlashcardTest Refactor

--- java/a1p2/app/src/main/java/org/example/Flashcard.java
+++ java/a1p2/app/src/main/java/org/example/Flashcard.java
@@ -3,8 +3,8 @@ package org.example;
  */
 public class Flashcard {
     //Attributes
-    public String question;
-    public String answer;
+    private String question;
+    private String answer;
 
     //Constructor
     public Flashcard(String question, String answer) {
