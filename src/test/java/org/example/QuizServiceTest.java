e2aae7947564c7525302e00fe09b71d09531c860
Sisi Wu <sw223hk@student.lnu.se>
2025-12-10 10:08:04 +0100

    testReturnCardQuestion() RED

--- src/test/java/org/example/QuizServiceTest.java
+++ src/test/java/org/example/QuizServiceTest.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 public class QuizServiceTest {
 
+    // Returns for next card from FlashcardDeck
+    @Test
+    void testReturnCardQuestion() {
+        // 1. Set up mock
+        FlashcardDeck mockFcDeck = Mockito.mock(FlashcardDeck.class);
+        Flashcard mockFc = new Flashcard("No problem in Swedish?", "Inga problem");
+
+        // Behavior: Tell mock to return mockFc when call from mockFcDeck
+        when(mockFcDeck.getNextFlashcard()).thenReturn(mockFc);
+
+        // 2. Run quizService to verify behavior
+        QuizService service = new QuizService(mockFcDeck);
+
+        Flashcard returnedCard = service.retrieveQuestion();
+
+        // Check if question from returnedCard is the same as mockFc
+        assertEquals(mockFc.getQuestion(), returnedCard.getQuestion());
+
+        // Verify that FlashcardDeck is called only once
+        verify(mockFcDeck, times(1)).getNextFlashcard();
+    }
 }
