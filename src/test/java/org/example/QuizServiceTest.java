c05f1147fbdd81b994a1ea7ed895991166d53dc2
Sisi Wu <sw223hk@student.lnu.se>
2025-12-15 13:45:05 +0100

    fix coverage test from QuizService

--- src/test/java/org/example/QuizServiceTest.java
+++ src/test/java/org/example/QuizServiceTest.java
@@ -61,7 +61,7 @@ public class QuizServiceTest {
     }
 
     @Test
-    void testScoreCorrect() {
+    void testSetUserCorrectly() {
         // Create new user with userScore
         User user = new User("Thomas", 5);
 
@@ -69,11 +69,65 @@ public class QuizServiceTest {
         QuizService service = new QuizService();
         service.setUser(user);
 
-        // Act
-        boolean result = service.checkScore(user, 5);
-
         // Check if returnedScore is the same as userScore
-        assertTrue(result);
+        assertEquals(user, service.getReturnedUser()); // compare returnedUser to user without Service
 
     }
+
+    // Test if set score correctly through QuizService 
+    @Test
+    void testSetScoreCorrectly() {
+        // Create new user with userScore
+        User user = new User("Thomas", 5);
+
+        // 2. Run quizService to verify behavior
+        QuizService service = new QuizService();
+        service.setScore(5); //
+
+        // Check if returnedScore is the same as userScore
+        assertEquals(user.getScore(), service.getReturnedScore());
+    }
+
+    // Test if check score correctly through QuizService 
+    @Test
+    void testCheckScoreCorrectly() {
+        // Create new user with userScore
+        User user = new User("Thomas", 5);
+
+        // 2. Run quizService to verify behavior
+        QuizService service = new QuizService();
+        service.setScore(5); //Check if the user's score is 5
+
+        boolean serviceScoreResult = service.checkScore(user, service.getReturnedScore()); // store result in serviceScoreResult
+
+        // Check if returnedScore is the same as userScore
+        assertTrue(serviceScoreResult);
+
+    }
+
+    // Test if check score correctly through QuizService 
+    @Test
+    void testGetReturnedQuestionCorrectly() {
+        // Set up mock
+        FlashcardDeck mockFcDeck = Mockito.mock(FlashcardDeck.class);
+        Flashcard mockFc = new Flashcard("Why in Swedish?", "Varför?");
+
+        // Expected Behavior: tell mock to return mockFc when call from mockFcDeck
+        when(mockFcDeck.getNextFlashcard()).thenReturn(mockFc);
+
+        // 2. Run quizService to verify behavior
+        QuizService service = new QuizService();
+        service.setFlashcardDeck(mockFcDeck);
+
+        // Act
+        String result = service.getReturnedQuestion();
+
+        // Check if question from returnedCard is the same as mockFc
+        assertEquals(mockFc.getQuestion(), result);
+
+        // Verify that FlashcardDeck is called only once
+        verify(mockFcDeck, times(1)).getNextFlashcard();
+
+    }
+
 }
